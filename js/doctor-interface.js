451a68aed9ac4560077b641a1818c394a856e2d6
Ryan <ryanjonesirl@gmail.com>
2017-04-21 12:14:55 -0700

    add the logic to pull back relevant information and append it to the screen

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,29 +2,22 @@ var apiKey = require('./../.env').apiKey;
 var Time = require('../js/time.js').timeModule;
 
 $(function() {
-  $("#currentTime").text(Time.getCurrentTime());
+  var time = new Time();
+  $("#currentTime").text(time.getCurrentTime());
   $("#search").click(function() {
     var userLocation = $("#userLocation").val();
     $.ajax({
-        type: "GET",
-        url: "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=" + api_key,
-        dataType: "json",
-        success: processJSON
-      });
-
-      function processJSON(json) {
-        var doctorInformationArray = new Array();
-        $(json).find("data").each(function() {
-          var doctorName = json.data[0].profile.first_name;
-
-          // var doctorInformation = {
-          //   "name": doctorName,
-          //   "location": doctorLocation
-          // }
-          $("#doctorList").append("<li>" + doctorName + "</li>");
-          // doctorInformationArray.push(doctorInformation);
-        });
-        // localStorage.setItem("doctorInformationArray", JSON.stringify(doctorInformationArray));
-      }
+      type: "GET",
+      url: "https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=" + apiKey,
+      dataType: "json",
+      success: processJSON
     });
+
+    function processJSON(json) {
+      for(var i = 0; i < json.data.length; i++){
+        var doctorName = json.data[i].profile.first_name;
+        $("#doctorList").append("<li><a href=''>" + doctorName + "</li>");
+      }
+    }
+  });
 });
